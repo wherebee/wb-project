6ae77405eca913f074ddfd3173d66df8db744181
Matthew Scott <gldnspud@gmail.com>
2011-06-12 19:07:51 -0700

    typo: TEMPLATE_CONTEXT_PREPROCESSORS -> TEMPLATE_CONTEXT_PROCESSORS

--- wbproject/settings.py
+++ wbproject/settings.py
@@ -20,7 +20,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-TEMPLATE_CONTEXT_PREPROCESSORS = (
+TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
